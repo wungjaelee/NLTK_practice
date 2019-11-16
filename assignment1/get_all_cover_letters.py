963468f17017fae1c96a474eaf36db17934f8660
Wung Jae Lee <wungjaelee@Wungs-MBP.attlocal.net>
2019-11-15 19:52:45 -0600

    modularized functions, fixed automatic collection of cvs from indeed, added some materials

--- assignment1/get_all_cover_letters.py
+++ assignment1/get_all_cover_letters.py
@@ -3,6 +3,14 @@ from urllib import request
 import re
 import sys
 
+STARTING_PHRASES = ['Dear Hiring Manager',
+                    'To Whom It May Concern',
+                    'Dear Director of Athletics',
+                    'Dear Chief Pilot and Hiring Manager',
+                    'Dear Director of Housing',
+                    'Dear School Board']
+ENDING_PHRASES = ['Sincerely']
+
 def get_all_cover_letter_urls(url):
     resp = request.urlopen(url)
     soup = BeautifulSoup(resp, from_encoding=resp.info().get_param('charset'))
@@ -14,10 +22,26 @@ def get_all_cover_letter_urls(url):
     return cover_letter_urls
 
 def parse_raw_text(raw_text, starting_phrase, ending_phrase):
-    return raw_text[raw_text.find(starting_phrase):raw_text.rfind(ending_phrase)]
+    start = raw_text.find(starting_phrase)
+    if start == -1:
+        return ""
+    end = raw_text.rfind(ending_phrase)
+    if end == -1:
+        return ""
+    return raw_text[start:end]
 
 def get_job_title(cv_url):
     return cv_url.split('/')[-1]
+
+def find_cover_letter_content(raw_text):
+    for starting_phrase in STARTING_PHRASES:
+        for ending_phrase in ENDING_PHRASES:
+            cv_content = parse_raw_text(raw_text, starting_phrase, ending_phrase)
+            if cv_content:
+                return cv_content
+    print("cover letter content was not detected: " + cv_url)
+    print("Consider modifying this code by adding appropriate starting_phrase or ending_phrase")
+    return ""
 
 def main():
     url = sys.argv[1]
@@ -26,10 +50,11 @@ def main():
         html = request.urlopen(cv_url).read().decode('utf8')
         raw = BeautifulSoup(html, 'html.parser').get_text()
 
-        cv_content = parse_raw_text(raw, 'Dear Hiring Manager,', 'Sincerely,')
+        cv_content = find_cover_letter_content(raw)
+
         job_title = get_job_title(cv_url)
 
-        with open(job_title + '.txt', 'w') as f:
+        with open('./cover_letters/' + job_title + '.txt', 'w') as f:
             f.write(cv_content)
 
 
--- assignment1/solution.py
+++ assignment1/solution.py
@@ -1,36 +1,85 @@
 import nltk
 from nltk.corpus.reader import CategorizedPlaintextCorpusReader
 
-fpsps = ["I", "me", "my"]
-fpsps_lower = ["i", "me", "my"]
-
-raw = open('/Users/wungjaelee/Documents/Squareteams/assignment1/cover_letters/academic-advisor.txt').read()
-
-words = nltk.word_tokenize(raw)
-#normalize words using lower()
-words = [word.lower() for word in words]
-
-text = nltk.Text(words)
-
-psps = [word for word in words if word in fpsps_lower]
-
-#count how many words are in the cover letter
-print(len(words))
-#count how many psps in the cover letter
-print(psps)
-#draw dispersion plot of psps in cover letter
-#text.dispersion_plot(first_person_singular_pronouns, ignore_case=True)
-#nltk.draw.dispersion.dispersion_plot(text, first_person_singular_pronouns, ignore_case=True)
+FPSPS = ["i", "my", "me"]
 
 
-fdist = nltk.FreqDist(psps)
-print(fdist.most_common(50))
-print(fdist.N())
+def build_cover_letter_corpus():
+    return CategorizedPlaintextCorpusReader('./cover_letters', '.*', cat_file='categories.txt')
+
+def draw_fpsps_dispersion_plot(cv_corpus, fileid):
+    file_text = normalize(cv_corpus.words(fileid))
+    nltk.draw.dispersion.dispersion_plot(file_text, FPSPS, ignore_case=True, title=fileid)
+
+def print_corpus_info(cv_corpus):
+    print('fileids: ', cv_corpus.fileids())
+    print('categories: ', cv_corpus.categories())
+
+def build_categories_fpsps_cfd(cv_corpus):
+    return nltk.ConditionalFreqDist((cat, w.lower()) for cat in cv_corpus.categories() for w in cv_corpus.words(categories=cat) if w.lower() in FPSPS)
+
+def build_files_fpsps_cfd(cv_corpus):
+    return nltk.ConditionalFreqDist((fileid.split('.')[0], w.lower()) for fileid in cv_corpus.fileids() for w in cv_corpus.words(fileid) if w.lower() in FPSPS)
+
+def word_percentage(target_word, nltk_text_obj):
+    try:
+        return 100 * nltk_text_obj.count(target_word) / len(nltk_text_obj)
+    except ZeroDivisionError:
+        return "NA"
+
+def normalize(words):
+    return [w.lower() for w in words]
+
+def extract_job_titles_from_file_ids(fileids):
+    return [fileid.split('.')[0] for fileid in fileids]
+
+
+def main():
+    cover_letter_corpus = build_cover_letter_corpus()
+
+    #show fpsps dispersion
+    #draw_fpsps_dispersion_plot(cover_letter_corpus, 'academic-advisor.txt')
+
+    #build ConditionalFreqDist
+    cfd_cat = build_categories_fpsps_cfd(cover_letter_corpus)
+    cfd_file = build_files_fpsps_cfd(cover_letter_corpus)
+
+
+    #tabulate cfd
+    cfd_cat.tabulate(conditions=cover_letter_corpus.categories()[:10])
+    cfd_file.tabulate(conditions=extract_job_titles_from_file_ids(cover_letter_corpus.fileids()[:10]))
+    print()
+
+
+    #fpsp percentages for each file and category
+    print("category\tI\tmy\tme")
+    for category in cover_letter_corpus.categories()[:10]:
+        nltk_text_obj = nltk.Text(normalize(cover_letter_corpus.words(categories=category)))
+        #print(len(nltk_text_obj))
+        print(category + '\t' + '\t'.join([str(word_percentage(fpsp, nltk_text_obj)) for fpsp in FPSPS]))
+
+    print()
+    print("job_title\tI\tmy\tme")
+    for fileid in cover_letter_corpus.fileids()[:10]:
+        nltk_text_obj = nltk.Text(normalize(cover_letter_corpus.words(fileids=fileid)))
+        print(fileid + '\t' + '\t'.join([str(word_percentage(fpsp, nltk_text_obj)) for fpsp in FPSPS]))
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+#fdist = nltk.FreqDist(psps)
+#print(fdist.most_common(50))
+#print(fdist.N())
 #fdist.plot()
 
-cv_corpus = CategorizedPlaintextCorpusReader('./cover_letters', '.*', cat_file='categories.txt')
-cfd_cat = nltk.ConditionalFreqDist((cat, w.lower()) for cat in cv_corpus.categories() for w in cv_corpus.words(categories=cat) if w.lower() in fpsps_lower)
-cfd_file = nltk.ConditionalFreqDist((fileid.split('.')[0], w.lower()) for fileid in cv_corpus.fileids() for w in cv_corpus.words(fileid) if w.lower() in fpsps_lower)
+#cv_corpus = CategorizedPlaintextCorpusReader('./cover_letters', '.*', cat_file='categories.txt')
+#cfd_cat = nltk.ConditionalFreqDist((cat, w.lower()) for cat in cv_corpus.categories() for w in cv_corpus.words(categories=cat) if w.lower() in fpsps_lower)
+#cfd_file = nltk.ConditionalFreqDist((fileid.split('.')[0], w.lower()) for fileid in cv_corpus.fileids() for w in cv_corpus.words(fileid) if w.lower() in fpsps_lower)
 #cfd = nltk.ConditionalFreqDist((w, fileid.split('.')[0]) for fileid in cv_corpus.fileids() for w in cv_corpus.words(fileid) if w.lower() in fpsps_lower)
 #cfd.plot()
 #cfd.tabulate()
